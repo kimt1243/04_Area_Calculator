54da0126958f647090f73c8bb427ba1988176a99
kimt1243 <100240773+kimt1243@users.noreply.github.com>
2023-08-10 10:47:00 +1200

    Finished?

--- 01_Base_v2.py
+++ 01_Base_v2.py
@@ -53,39 +53,48 @@ def valid_checker(question, search_list, error):
         for item in search_list:
             if response == item[0] or response == item:
                 return item
+
         # Prints error code if the user answer is not a valid answer.
         print(error)
         print()
 
 
-# input checker, used for components consisting of checking integers
+# int/float checker, used for components consisting of checking integers
 def input_checker(question, allow_floats=False):
     while True:
         response = input(question)
 
-        # returns continuous mode
+        # returns continuous mode.
         if response == '' and allow_floats is False:
             return response
-        # returns exit code
+
+        # returns the exit code when user enters 'xxx'
         elif response == 'xxx':
             return 'xxx'
-        
+
         elif response != "":
             try:
                 if allow_floats is True:
                     response = float(response)
+
                 elif allow_floats is False:
                     response = int(response)
+
+                # if response is lower than 1, print number error.
                 if response < 1:
+                    print()
                     print("Please input a valid number greater than or equal to 1\n")
                 else:
                     return response
+            # if user answer is not an integer / number, print ValueError
             except ValueError:
+                print()
                 print("<Error> That is an invalid integer / number!\n")
 
 
 # Question generator that is based on the difficulty the user chose.
 def question_gen(difficulty):
+
     # Available variables that will be used for the areas.
     height = random.randint(1, 10)
     width = random.randint(1, 10)
@@ -111,14 +120,18 @@ def question_gen(difficulty):
         area = 3.14 * radius ** 2
         question = f"Find the area of a circle if the radius is {radius}, (PI is set as 3.14): "
 
+    # returns the area and question.
     return area, question
 
 
 # Main Function
 def main():
+
     # Available Lists
     yes_no_list_checker = {"yes", "no", "xxx"}
     difficulty_checker = {"easy", "medium", "hard", "xxx"}
+
+    # Available variables
     questions_played = 0
     question_correct = 0
     question_wrong = 0
@@ -129,22 +142,29 @@ def main():
     print("====================================")
     print()
 
-    # Ask if the player wants to see the instructions
+    # Ask if the player wants to see the instructions.
     played_before = valid_checker("Would you like to see the instructions? ", yes_no_list_checker,
                                   "Please type either yes or no ('xxx' to exit)")
     print()
+    # if user enters 'yes', print instructions.
     if played_before == "yes":
         instructions()
+
+    # if the user answer is 'xxx' when they are asked if they want to see the instructions, exit the code.
     elif played_before == "xxx":
         print("You have chosen 'xxx'. Thank you for playing!")
         exit()
 
-    # Ask if the player wants to see the formula list
+    # Ask if the player wants to see the formula list.
     formula_check = valid_checker("Would you like to see the formula list? ", yes_no_list_checker,
                                   "Please type a either yes or no ('xxx' to exit)")
     print()
+
+    # if user enters 'yes', print formula.
     if formula_check == "yes":
         display_formula_instructions()
+
+    # if the user answer is 'xxx' when they are asked if they want to see the formula, exit the code.
     elif formula_check == "xxx":
         print("You have chosen 'xxx'. Thank you for playing!")
         exit()
@@ -153,10 +173,14 @@ def main():
     user_difficulty = valid_checker("Which difficulty would you like to play? ", difficulty_checker,
                                     "Please type a valid difficulty (easy, medium, hard)")
     print()
+
+    # if the user answer is 'xxx' when they are asked to enter the difficulty of the question
+    # they want to play, exit the code.
     if user_difficulty == "xxx":
         print("You have chosen 'xxx'. Thank you for playing!")
         exit()
 
+    # prints which difficulty the user has chosen.
     print("-----------------------------------")
     print(f"You have chosen the {user_difficulty} difficulty!")
     print("------------------------------------")
@@ -165,42 +189,54 @@ def main():
     # Ask the player how many questions they want to play
     questions = input_checker('How many QUESTIONS would you like to play? <enter> for continuous mode: ')
 
+    # if the user answer is 'xxx' when they are asked to enter the amount of question they want to play, exit the code.
     if questions == 'xxx':
-        print('Thanks for playing! ')
+        print("You have chosen 'xxx'. Thank you for playing!")
         exit()
 
     while True:
         print()
+        # if the user chooses <enter> for number of questions, print Continuous questions.
         if questions == "":
             heading = f"Continuous Mode: QUESTION {questions_played + 1}"
+
+        # if the user chooses a valid number for number of questions, print Questions.
         else:
             heading = f"!QUESTION {questions_played + 1} of {questions}!"
         print(heading)
 
         area, math_question = question_gen(user_difficulty)
 
+        # checks the type of value of answer, sets input_checker accordingly.
         if isinstance(area, float):
             user_answer = input_checker(math_question, True)
         elif isinstance(area, int):
             user_answer = input_checker(math_question, False)
 
+        # if the users answer is 'xxx' but the user has not played more than 1 question, make the user play one question
         if user_answer == "xxx" and questions_played == 0:
             print("You have to play at least one QUESTION!")
             continue
 
+        # If the user answer is 'xxx' and they have played more than one question, exit game.
         elif user_answer == "xxx":
             print("\nYou have chosen to exit the Quiz. Thank you for playing!\n")
             break
 
+        # If the user answer is the same as the area, + 1 to the question correct value.
         if user_answer == area:
             print("Correct! You calculated the area correctly!")
             question_correct += 1
+
+        # If the user answer is not the same as the area, + 1 to the question wrong value.
         else:
             print("Incorrect. The correct answer is", area)
             question_wrong += 1
 
         questions_played += 1
 
+        # If the amount of questions played is the same as the amount of questions
+        # that the user wanted to play at the start, break game.
         if questions_played == questions:
             break
 
@@ -218,15 +254,12 @@ if game_stats == "exit":
 else:
     stats_question_correct, stats_question_wrong = game_stats
 
-    # Calculate Game Stats
+    # Calculate Game Stats.
     stats_question_played = stats_question_correct + stats_question_wrong
 
-    if stats_question_played == 0:
-        percent_correct = 0.0
-        percent_wrong = 0.0
-    else:
-        percent_correct = stats_question_correct / stats_question_played * 100
-        percent_wrong = stats_question_wrong / stats_question_played * 100
+    # Calculate percentage stats.
+    percent_correct = stats_question_correct / stats_question_played * 100
+    percent_wrong = stats_question_wrong / stats_question_played * 100
 
     # Displays game stats with % values to the nearest whole number
     print("\n===== Game Statistics =====")
